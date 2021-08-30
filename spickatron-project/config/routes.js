091d1c6b3a9cd2a8e221f3e3d2c6cbd11b30a58e
Brett Grentell <doublewish@spickatron.com>
2021-08-30 21:27:48 +1000

    Added chatapp page

--- spickatron-project/config/routes.js
+++ spickatron-project/config/routes.js
@@ -16,6 +16,8 @@ module.exports.routes = {
   'GET /':                   { action: 'view-homepage-or-redirect' },
   'GET /welcome/:unused?':   { action: 'dashboard/view-welcome' },
 
+  'GET /chatapp':            { view: 'pages/chatapp'},
+
   'GET /faq':                { action:   'view-faq' },
   'GET /legal/terms':        { action:   'legal/view-terms' },
   'GET /legal/privacy':      { action:   'legal/view-privacy' },
